6941898b28fa9c378629f4e66f03cddd53dd3830
Ahmed Saad <ahmedsaad112004cr@gmail.com>
2022-07-07 16:49:03 +0200

    Modified the functionality of the dropdown menu

--- js/main.js
+++ js/main.js
@@ -8,7 +8,6 @@ const header = document.querySelector("header")
 const lists = document.querySelectorAll(".list-container");
 
 
-
 openMenuBtn.addEventListener("click", () => {
   menu.style.transform = "translateX(0)";
   header.style.setProperty("--width", "100%")
@@ -21,12 +20,48 @@ openMenuBtn.addEventListener("click", () => {
 
 
 lists.forEach(element => {
-  element.addEventListener("click", _ => {
+  element.children[0].addEventListener("click", _ => {
 
     element.classList.toggle("expanding");
     element.querySelector("svg").classList.toggle("up-down")
     element.children[1].classList.toggle("expanding");
+    document.addEventListener("click", (event) => {
+      let result = isDescendant(element, event.target);
 
+      if (result == 0) {
+        element.classList.remove("expanding");
+        element.children[1].classList.remove("expanding");
+      }
+    });
   });
 
-})
+});
+
+
+
+
+function isDescendant(parent, element) {
+
+  if (parent.isSameNode(element)) {
+    return 1;
+  }
+
+  let found = false;
+  function isChild(childs) {
+    for (let i of childs) {
+      if (i.isSameNode(element)) {
+        found = true;
+        return 2;
+      }
+      if (i.children) {
+        isChild(i.children)
+        if (found) {
+          return 2;
+        }
+      }
+    }
+    return 0;
+  }
+
+  return isChild(parent.children);
+}
